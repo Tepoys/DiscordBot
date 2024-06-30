0de9b700f8fa4721773906f0278dfc8219c2b933
Bida L <bida2023@gmail.com>
2024-06-30 19:27:37 -0400

    Finished player money interaction with shop and earning money

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from database import link_connection
+from shop import Shop
 
 
 class Player:
@@ -26,6 +27,37 @@ class Player:
         bucks = fetch[0]
         return bucks
 
+    def change_bucks(self, change_amount: int) -> None:
+        Player.cur.execute("UPDATE players SET bucks = bucks + ? WHERE player_id = ?",
+                           [(change_amount,), (self.id,)])
+
+        Player.con.commit()
+
+    def buy(self, item_name: str, store_id: str, shops: dict[str, Shop]) -> bool:
+        """
+        Buys item_name from store_id
+        :param shops: list of shops to find item in
+        :param item_name: Name of the item to buy
+        :param store_id: The store of where the item is
+        :return: true if the item was successfully bought and false otherwise
+        """
+        store = shops.get(store_id)
+        if store is None:
+            print(f'Store {store_id} does not exist')
+            return False
+
+        item_cost: int = store.cost(item_name)
+
+        if item_cost is None:
+            print(f'Item {item_name} does not exist in store {store_id}')
+            return False
+
+        self.change_bucks(item_cost*-1)
+
+        return True
+
+def cleanup_player() -> None:
+    Player.con.close()
 
 def main() -> None:
     Player(id='0')
