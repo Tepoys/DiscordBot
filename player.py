641a529ce3f1390c4475a019fbb1ebe0f28173d3
Bida L <bida2023@gmail.com>
2024-06-30 14:30:35 -0400

    Finished player-database bucks fetching

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from database import link_connection
 
 
 class Player:
+    default_bucks = 0
     con, cur = link_connection()
 
     def __init__(self, *, id: str = None):
@@ -9,10 +10,25 @@ class Player:
             raise TypeError('Id is None')
 
         self.id = id
+        self.bucks = self.get_bucks()
+        print(self.bucks)
+
+    def get_bucks(self) -> int:
+        res = Player.cur.execute("""
+            SELECT bucks FROM players WHERE player_id = ? LIMIT 1
+        """, (self.id,))
+        fetch = res.fetchone()
+        if fetch is None:
+            Player.cur.execute("INSERT INTO players (player_id, bucks) VALUES (?, ?)", (self.id, 0))
+            Player.con.commit()
+            return 0
+
+        bucks = fetch[0]
+        return bucks
 
 
 def main() -> None:
-    pass
+    Player(id='0')
 
 
 if __name__ == '__main__':
